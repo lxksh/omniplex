29954943e398671873404c7071f55b63b9937912
lxksh <lakshvijay04@gmail.com>
2025-08-02 02:47:39 +0530

    Completed

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -13,8 +13,8 @@ export const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 export default app;
